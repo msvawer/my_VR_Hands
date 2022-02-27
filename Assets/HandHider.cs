4235fa0b0142aaa43b5f729c04acb18426bdf687
MichVawer <michelle.vawer@icloud.com>
2022-02-27 11:31:11 -0500

    h

--- Assets/HandHider.cs
+++ Assets/HandHider.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class HandHider : MonoBehaviour
+public class HandHider : MonoBehaviour  
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject handObject = null;
+    private HandPhysics handPhysics = null;
+    private XRDirectInteractor Interactor = null;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+
+        handPhysics = handObject.GetComponent<HandPhysics>();
     }
 }
